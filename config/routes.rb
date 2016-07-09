58e8551f25b4916393d007b7ff22ae1b7e8b363b
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-07-09 14:19:14 -0700

    created album, track and bands models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  # root to: redirect("/bands")
-  resources :users, only: [:new, :create, :show]
-  resource :session, only: [:new, :create, :destroy]
+  root to: redirect("/bands")
+
+  resource :session, only: [:create, :destroy, :new]
+  resources :users, only: [:create, :new, :show] do
+    get :activate, on: :collection
+  end
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:create, :destroy, :edit, :show, :update] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, only: [:create, :destroy, :edit, :show, :update]
 end
---
+++ db/migrate/20160709210132_create_albums.rb
@@ -0,0 +1,14 @@
+class CreateAlbums < ActiveRecord::Migration
+  def change
+    create_table :albums do |t|
+      t.string :name, null: false
+      t.integer :band_id, null: false
+      t.integer :year, null: false
+      t.boolean :live, default: false, null: false
+
+
+      t.timestamps null: false
+    end
+    add_index :albums, [:name, :band_id], unique: true
+  end
+end
